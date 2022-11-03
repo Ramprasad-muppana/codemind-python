c754e4ef530c993758a1e60a118607bdccf5dadf
Ramprasad <83056522+Ramprasad-muppana@users.noreply.github.com>
2022-11-03 22:13:04 +0530

    a,b=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) c=[] for i in l1:     if i in l2 and i not in c:         c.append(i) print(len(c))

---
+++ Count_elements_I.py
@@ -0,0 +1,8 @@
+a,b=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+c=[]
+for i in l1:
+    if i in l2 and i not in c:
+        c.append(i)
+print(len(c))
