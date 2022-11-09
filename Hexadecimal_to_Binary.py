95b7d47ff9935ea7b112035a503a0bce6aed99c6
Ramprasad <83056522+Ramprasad-muppana@users.noreply.github.com>
2022-11-09 08:50:43 +0530

    for i in range(int(input())):     n=input()     s=""     for i in n:         if(i=='0'):             s+='0000'         elif(i=='1'):             s+="0001"         elif(i=='2'):             s+="0010"         elif(i=='3'):             s+="0011"         elif(i=='4'):             s+="0100"         elif(i=='5'):             s+="0101"         elif(i=='6'):             s+="0110"         elif(i=='7'):             s+="0111"         elif(i=='8'):             s+="1000"         elif(i=='9'):             s+="1001"         elif(i=='A'):             s+="1010"         elif(i=='B'):             s+="1011"         elif(i=='C'):             s+="1100"         elif(i=='D'):             s+="1101"         elif(i=='E'):             s+="1110"         elif(i=='F'):             s+="1111"     print(s)

---
+++ Hexadecimal_to_Binary.py
@@ -0,0 +1,37 @@
+for i in range(int(input())):
+    n=input()
+    s=""
+    for i in n:
+        if(i=='0'):
+            s+='0000'
+        elif(i=='1'):
+            s+="0001"
+        elif(i=='2'):
+            s+="0010"
+        elif(i=='3'):
+            s+="0011"
+        elif(i=='4'):
+            s+="0100"
+        elif(i=='5'):
+            s+="0101"
+        elif(i=='6'):
+            s+="0110"
+        elif(i=='7'):
+            s+="0111"
+        elif(i=='8'):
+            s+="1000"
+        elif(i=='9'):
+            s+="1001"
+        elif(i=='A'):
+            s+="1010"
+        elif(i=='B'):
+            s+="1011"
+        elif(i=='C'):
+            s+="1100"
+        elif(i=='D'):
+            s+="1101"
+        elif(i=='E'):
+            s+="1110"
+        elif(i=='F'):
+            s+="1111"
+    print(s)        
